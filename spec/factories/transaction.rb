f074bd916fd1cb599f8a4c5420a75aed0d555584
Christina Delpone <cdelpone@gmail.com>
2021-09-15 11:30:30 -0600

    Increase factory bot ranges

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     association :invoice
     credit_card_number { Faker::Number.number(digits: 16).to_s }
     result { [0, 1].sample }
-    id { Faker::Number.unique.within(range: 1..100) }
+    id { Faker::Number.unique.within(range: 1..100000) }
   end
 end
