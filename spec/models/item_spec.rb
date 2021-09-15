80cb843b87e0d23b2348c9ce310003462f0d6f8f
Tanner Dale <tanner@tannerdale.com>
2021-09-14 21:16:52 -0500

    Remove unndeeded create

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 # rspec spec/models/item_spec.rb
 RSpec.describe Item, type: :model do
   describe 'relationships' do
-    let(:item) { create :item }
     it { should belong_to(:merchant) }
   end
 end
