f42d5250a7e71baa5452637670cfc1ceb4e81516
Christina Delpone <cdelpone@gmail.com>
2021-09-14 21:55:07 -0600

    Test dashboard show returns top 5 customers

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -3,6 +3,39 @@ require 'rails_helper'
 RSpec.describe 'Merchant Dashboard Show Page' do
   describe 'Merchant Dashboard Show Page' do
     let!(:merchant1) { create(:merchant) }
+    let!(:customer) { create :customer }
+    let!(:customer2) { create :customer }
+    let!(:customer3) { create :customer }
+    let!(:customer4) { create :customer }
+    let!(:customer5) { create :customer }
+    let!(:customer6) { create :customer }
+    let!(:item1) { create :item, { merchant_id: merchant1.id } }
+    let!(:item2) { create :item, { merchant_id: merchant1.id } }
+    let!(:item3) { create :item, { merchant_id: merchant1.id } }
+    let!(:invoice1) { create :invoice, { customer_id: customer.id} }
+    let!(:invoice2) { create :invoice, { customer_id: customer2.id} }
+    let!(:invoice3) { create :invoice, { customer_id: customer2.id} }
+    let!(:invoice8) { create :invoice, { customer_id: customer2.id} }
+    let!(:invoice4) { create :invoice, { customer_id: customer3.id} }
+    let!(:invoice5) { create :invoice, { customer_id: customer4.id} }
+    let!(:invoice6) { create :invoice, { customer_id: customer5.id} }
+    let!(:invoice7) { create :invoice, { customer_id: customer6.id} }
+    let!(:transaction1) { create :transaction, { invoice_id: invoice1.id, result: 1} }
+    let!(:transaction2) { create :transaction, { invoice_id: invoice2.id, result: 0 } }
+    let!(:transaction3) { create :transaction, { invoice_id: invoice3.id, result: 0 } }
+    let!(:transaction4) { create :transaction, { invoice_id: invoice4.id, result: 0 } }
+    let!(:transaction5) { create :transaction, { invoice_id: invoice5.id, result: 0 } }
+    let!(:transaction6) { create :transaction, { invoice_id: invoice6.id, result: 0 } }
+    let!(:transaction7) { create :transaction, { invoice_id: invoice7.id, result: 0 } }
+    let!(:transaction8) { create :transaction, { invoice_id: invoice8.id, result: 0 } }
+    let!(:inv_item1) { create :invoice_item, { item_id: item1.id, invoice_id: invoice1.id } }
+    let!(:inv_item2) { create :invoice_item, { item_id: item2.id, invoice_id: invoice2.id } }
+    let!(:inv_item3) { create :invoice_item, { item_id: item3.id, invoice_id: invoice3.id } }
+    let!(:inv_item4) { create :invoice_item, { item_id: item3.id, invoice_id: invoice4.id } }
+    let!(:inv_item5) { create :invoice_item, { item_id: item3.id, invoice_id: invoice5.id } }
+    let!(:inv_item7) { create :invoice_item, { item_id: item3.id, invoice_id: invoice6.id } }
+    let!(:inv_item8) { create :invoice_item, { item_id: item3.id, invoice_id: invoice7.id } }
+    let!(:inv_item9) { create :invoice_item, { item_id: item3.id, invoice_id: invoice8.id } }
 
     before :each do
       visit merchant_dashboard_path(merchant1.id)
@@ -24,6 +57,23 @@ RSpec.describe 'Merchant Dashboard Show Page' do
         click_on 'Invoices'
 
         expect(current_path).to eq(merchant_invoices_path(merchant1))
+      end
+    end
+
+    describe 'statistics - favorite customers' do
+      it 'shows names of the top five customers with successful transactions' do
+        fav2 = "#{customer2.first_name} #{customer2.last_name}"
+        fav3 = "#{customer3.first_name} #{customer3.last_name}"
+        fav4 = "#{customer4.first_name} #{customer4.last_name}"
+        fav5 = "#{customer5.first_name} #{customer5.last_name}"
+        fav6 = "#{customer6.first_name} #{customer6.last_name}"
+
+        expect(fav2).to appear_before(fav3)
+        expect(page).to have_content(fav2)
+        expect(page).to have_content(fav3)
+        expect(page).to have_content(fav4)
+        expect(page).to have_content(fav5)
+        expect(page).to have_content(fav6)
       end
     end
   end
