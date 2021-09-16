c829c5f58c0c2636a791f9dae49b5b037f5d7e6d
matthewjholmes <ilookup@gmail.com>
2021-09-15 19:39:33 -0600

    Update tests after changing Merchant default status to disabled

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,28 +6,28 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should validate_presence_of :name }
 
-    it 'validates default enabled status' do
+    it 'validates default disabled status' do
       merchant = create(:merchant)
-      expect(merchant.status).to eq("enabled")
+      expect(merchant.status).to eq("disabled")
     end
   end
 
   describe 'class methods/scopes' do
     describe 'by status' do
       before :each do
-        @merch1 = create :merchant, { status: 'disabled'}
+        @merch1 = create :merchant, { status: 'enabled'}
         @merch2 = create :merchant
-        @merch3 = create :merchant, { status: 'disabled'}
+        @merch3 = create :merchant, { status: 'enabled'}
         @merch4 = create :merchant
       end
 
-      it 'has enabled' do
-        expect(Merchant.by_status('enabled')).to eq([@merch2, @merch4])
+      it 'has disabled' do
+        expect(Merchant.by_status('disabled')).to eq([@merch2, @merch4])
       end
 
-      it 'has disabled' do
-        expect(Merchant.by_status('disabled')).to eq([@merch1, @merch3])
+      it 'has enabled' do
+        expect(Merchant.by_status('enabled')).to eq([@merch1, @merch3])
       end
     end
-  end
+  end 
 end
