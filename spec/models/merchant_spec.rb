8e21085db7dd8187faca6f71aa054a80b23c6536
Tanner Dale <tanner@tannerdale.com>
2021-09-15 19:29:48 -0500

    Add test for by status

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Merchant, type: :model do
       expect(merchant.status).to eq("enabled")
     end
   end
+
+  describe 'class methods/scopes' do
+    describe 'by status' do
+      before :each do
+        @merch1 = create :merchant, { status: 'disabled'}
+        @merch2 = create :merchant
+        @merch3 = create :merchant, { status: 'disabled'}
+        @merch4 = create :merchant
+      end
+
+      it 'has enabled' do
+        expect(Merchant.by_status('enabled')).to eq([@merch2, @merch4])
+      end
+
+      it 'has disabled' do
+        expect(Merchant.by_status('disabled')).to eq([@merch1, @merch3])
+      end
+    end
+  end
 end
