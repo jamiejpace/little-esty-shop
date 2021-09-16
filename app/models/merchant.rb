751fbaea5f1475b8979390506da52a0eb2c44ad3
Tanner Dale <tanner@tannerdale.com>
2021-09-16 18:21:25 -0500

    it aint too hot

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,9 @@ class Merchant < ApplicationRecord
   scope :get_highest_id, -> {
     maximum(:id) || 1
   }
+
+  scope :top_five_merchants, -> {
+    select("SUM(invoice_items.quantity * invoice_items.unit_price) AS total").distinct.joins(items: { invoices: :transactions } )
+    .where("transactions.result = ?", 0).group(:id).order(total: :desc).limit(5).pluck(:name, "SUM(invoice_items.quantity * invoice_items.unit_price)")
+  }
 end
