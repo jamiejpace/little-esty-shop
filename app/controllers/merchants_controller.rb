0126c3b5a4c88db8515f5c1c087cda8c386bd4fa
Jamie Pace <jamiejpace@gmail.com>
2021-09-16 19:05:40 -0600

    Update show view and action with refactored items method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,6 +2,6 @@ class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @customers = Invoice.merchant_fav_customers(@merchant)
-    @invoices = InvoiceItem.merch_items_ship_ready(@merchant)
+    @items = Item.merch_items_ship_ready(@merchant)
   end
 end
