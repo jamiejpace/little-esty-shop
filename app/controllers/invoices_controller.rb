a7acb74f209102dc5f4b90da935ba4051fd7c7fb
Christina Delpone <cdelpone@gmail.com>
2021-09-18 15:15:22 -0600

    Add inv_item update action, inv private current merch method

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,11 +1,17 @@
 class InvoicesController < ApplicationController
+  before_action :current_merchant
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @invoices = Invoice.for_merchant(@merchant)
   end
 
   def show
     @invoice = Invoice.find(params[:id])
     @invoice_items = @invoice.invoice_items
+  end
+
+  private
+  def current_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
