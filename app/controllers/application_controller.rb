94b2f9237cfdfa8963bfdc6c5758966ded4b994e
matthewjholmes <ilookup@gmail.com>
2021-09-18 15:57:43 -0600

    API tweaks; need to move everything off of ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,26 @@
 class ApplicationController < ActionController::Base
 
-  before_action :set_github_info, only: :index
+  # before_action :set_github_info, only: :index
 
   def set_github_info
     conn = Faraday.new('https://api.github.com')
+
     repo_response = conn.get('/repos/TannerDale/little-esty-shop')
-    session[:repo] = JSON.parse(repo_response.body, symbolize_names: true)[:name]
-    contrib_response = conn.get('https://api.github.com/repos/TannerDale/little-esty-shop/contributors')
-    parsed_contributors = JSON.parse(contrib_response.body, symbolize_names: true)
+    parsed_repo = parse(repo_response)
+    session[:repo] = parsed_repo[:name]
+
+    contrib_response = conn.get('/repos/TannerDale/little-esty-shop/contributors')
+    parsed_contributors = parse(contrib_response)
     session[:user_names] = curate_names(parsed_contributors)
     session[:commits] = sum_contributions(parsed_contributors)
-    # @pull_requests =
+
+    pulls_response = conn.get('/repos/TannerDale/little-esty-shop/pulls?state=closed&per_page=100')
+    parsed_pulls = parse(pulls_response)
+    session[:pulls_count] = parsed_pulls.count
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def curate_names(contributors)
