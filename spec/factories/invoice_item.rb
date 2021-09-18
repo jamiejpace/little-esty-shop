09ddddeebda5bfa7967bd0df85200f6bcdaad09d
Tanner Dale <tanner@tannerdale.com>
2021-09-18 12:48:45 -0500

    Reduce quantity, unit_price upper limits

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :invoice_item, class: InvoiceItem do
     association :item
     association :invoice
-    quantity { Faker::Number.within(range: 1..100000) }
-    unit_price { Faker::Number.within(range: 1..100000) }
+    quantity { Faker::Number.within(range: 1..1000) }
+    unit_price { Faker::Number.within(range: 1..1000) }
     status { [0, 1, 2].sample }
     id { Faker::Number.unique.within(range: 1..1000000) }
   end
