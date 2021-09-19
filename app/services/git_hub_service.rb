e9205eca91a70bede698be62b9a826a1353c03f8
Jamie Pace <jamiejpace@gmail.com>
2021-09-19 15:23:31 -0600

    Refactor github service

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -1,4 +1,7 @@
+require 'set'
+
 class GitHubService
+  TURING_STAFF = %w(BrianZanti timomitchel scottalexandra jamisonordway)
   class << self
     def repo_info
       response = conn.get('/repos/tannerdale/little-esty-shop')
@@ -18,9 +21,40 @@ class GitHubService
       parse_data(response)[:name]
     end
 
-    def repo_contributors(repo_name)
+    def repo_contributors
       sleep 5
       response = conn.get('/repos/tannerdale/little-esty-shop/contributors')
+      parse_data(response)
+    end
+
+    def contributor_names
+      all_contribs = repo_contributors.map do |contrib|
+        contrib[:login]
+      end
+      all_contribs - TURING_STAFF
+    end
+
+    def names_and_commits
+      contributor_names.map do |name|
+        count = filter_results(name).length
+        "#{name} with #{count} commits"
+      end
+    end
+
+    def repo_commits(name)
+      response = conn.get("/repos/tannerdale/little-esty-shop/commits?author=#{name}&per_page=100&page_count=2")
+      Set.new(parse_data(response))
+    end
+
+    def filter_results(name)
+      repo_commits(name).delete_if do |result|
+        result[:commit][:message].start_with?("Merge")
+      end
+    end
+
+    def repo_pulls
+      #need to account for second page of results
+      response = conn.get('/repos/tannerdale/little-esty-shop/pulls?state=closed&per_page=100')
       parse_data(response)
     end
   end
