659b78cc8995b1e496378c38884963ca5e57e536
Jamie Pace <jamiejpace@gmail.com>
2021-09-19 11:15:26 -0600

    Refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,26 @@
 class ApplicationController < ActionController::Base
   # before_action :set_github_info, only: :index
 
-  def set_github_info
-    conn = Faraday.new('https://api.github.com')
-
-    repo_response = conn.get('/repos/TannerDale/little-esty-shop')
-    parsed_repo = parse(repo_response)
-    session[:repo] = parsed_repo[:name]
-
-    contrib_response = conn.get('/repos/TannerDale/little-esty-shop/contributors')
-    parsed_contributors = parse(contrib_response)
-    session[:user_names] = curate_names(parsed_contributors)
-    session[:commits] = sum_contributions(parsed_contributors)
-
-    pulls_response = conn.get('/repos/TannerDale/little-esty-shop/pulls?state=closed&per_page=100')
-    parsed_pulls = parse(pulls_response)
-    session[:pulls_count] = parsed_pulls.count
+  def self.set_github_info
+    git_hub_info = GitHubFacade.fetch_git_hub_info
+    require 'pry'; binding.pry
+    # conn = Faraday.new('https://api.github.com')
+    # repo_response = conn.get('/repos/TannerDale/little-esty-shop')
+    # repo_response = Faraday.get('https://api.github.com/repos/tannerdale/little-esty-shop')
+    # parsed_repo = parse(repo_response)
+    # session[:repo] = parsed_repo[:name]
+    #
+    # contrib_response = conn.get('/repos/TannerDale/little-esty-shop/contributors')
+    # parsed_contributors = parse(contrib_response)
+    # session[:user_names] = curate_names(parsed_contributors)
+    # session[:commits] = sum_contributions(parsed_contributors)
+    #
+    # pulls_response = conn.get('/repos/TannerDale/little-esty-shop/pulls?state=closed&per_page=100')
+    # parsed_pulls = parse(pulls_response)
+    # session[:pulls_count] = parsed_pulls.count
   end
 
-  def parse(response)
+  def self.parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 
