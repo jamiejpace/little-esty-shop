e650301f2f913bcf4fc4fd4ecacf3cba382bbe6c
Tanner Dale <tanner@tannerdale.com>
2021-09-20 12:01:42 -0500

    Refactor top_5_customers to use new scope, refactor layout

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,12 +3,19 @@ class Customer < ApplicationRecord
 
   has_many :invoices, dependent: :destroy
   has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
-  scope :top_5_customers, -> {
-    full_names.joins(:transactions).merge(Transaction.successful)
-    .group(:id).order(count: :desc).limit(5)
-    .select(:first_name, :last_name, "COUNT(transactions.id) AS transaction_count")
-  }
+  def self.top_5_customers
+    joins(:transactions)
+    .merge(Transaction.successful)
+    .group(:id)
+    .limit(5)
+    .merge(Invoice.transactions_count)
+    .order(count: :desc)
+    .full_names
+  end
 
   scope :full_names, -> {
     select("customers.first_name || ' ' || customers.last_name AS customer_name")
