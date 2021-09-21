0cd1dbdffacf43fde25ad6b7f72aeb3594f557a3
Tanner Dale <tanner@tannerdale.com>
2021-09-21 16:15:14 -0500

    Update api classes to client/service

--- app/services/git_hub_service.rb
+++ app/services/git_hub_service.rb
@@ -1,48 +1,48 @@
-require 'set'
-
 class GitHubService
   TURING_STAFF = %w(BrianZanti timomitchel scottalexandra jamisonordway)
-  class << self
-    def repo_info
-      response = conn.get('/repos/tannerdale/little-esty-shop')
-      parse_data(response)
-    end
 
-    def conn
-      Faraday.new('https://api.github.com')
-    end
+  def initialize(repo_name)
+    @repo_name = repo_name
+  end
 
-    def parse_data(response)
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  def repo_names_and_commits
+    contributor_commits
+  end
 
-    def repo_name
-      response = conn.get('/repos/tannerdale/little-esty-shop')
-      parse_data(response)[:name]
-    end
+  def repo_name
+    repo_name_info
+  end
 
-    def contribution_stats
-      response = conn.get('/repos/tannerdale/little-esty-shop/stats/contributors')
-      Set.new(parse_data(response))
+  def pulls_count
+    pulls_request_info.count do |pull|
+      !TURING_STAFF.include?(pull[:user][:login])
     end
+  end
 
-    def commits_by_contributor
-      contribution_stats.filter_map do |person|
-        username = person[:author][:login]
-        [username, person[:total]] unless TURING_STAFF.include?(username)
-      end.to_h
-    end
+  def commits_by_contributor
+    contribution_stats.filter_map do |person|
+      username = person[:author][:login]
+      [username, person[:total]] unless TURING_STAFF.include?(username)
+    end.to_h
+  end
 
-    def names_and_commits
-      commits_by_contributor.map do |name, count|
-        "#{name} with #{count} commits"
-      end
+  def names_and_commits
+    commits_by_contributor.map do |name, count|
+      "#{name} with #{count} commits"
     end
+  end
 
-    def repo_pulls
-      # need to account for second page of results
-      response = conn.get('/repos/tannerdale/little-esty-shop/pulls?state=closed&per_page=100')
-      parse_data(response)
-    end
+  private
+
+  def contributor_commits
+    @contributor_commits ||= GitHubClient.names_and_commits
+  end
+
+  def repo_name_info
+    @repo_name ||= GitHubClient.repo_name
+  end
+
+  def pulls_request_info
+    @pulls_count ||= GitHubClient.repo_pulls
   end
 end
