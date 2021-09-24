ea9116eace21d3d6dc7e680482d1b05f9b13a7ab
Jamie Pace <jamiejpace@gmail.com>
2021-09-24 16:44:58 -0600

    Create bulk discounts controller, add index action

---
+++ app/controllers/bulk_discounts_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class BulkDiscountsController < ApplicationController
+  before_action :current_merchant
+
+  def index
+    @bulk_discounts = BulkDiscount.all
+  end
+end
