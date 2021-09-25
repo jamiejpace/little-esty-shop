6ba49674bed73e7ce95c74de95d7e64a268670e1
Jamie Pace <jamiejpace@gmail.com>
2021-09-25 17:44:34 -0600

    Refactor bulk discount index action and add destroy action

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,7 +4,7 @@ class BulkDiscountsController < ApplicationController
   before_action :current_merchant
 
   def index
-    @bulk_discounts = BulkDiscount.all
+    @bulk_discounts = @merchant.bulk_discounts.all
     @holidays = BulkDiscountFacade.holidays
   end
 
@@ -20,12 +20,16 @@ class BulkDiscountsController < ApplicationController
       flash[:alert] = 'Unable to Create New Discount - Please Try Again'
       redirect_to new_merchant_bulk_discount_path(@merchant)
     end
+  end
 
+  def destroy
+    BulkDiscount.find(params[:id]).destroy
+    redirect_to merchant_bulk_discounts_path(@merchant)
   end
 
   private
 
   def bulk_discount_params
-    params.permit(:percentage_discount, :quantity_threshold)
+    params.permit(:name, :percentage_discount, :quantity_threshold)
   end
 end
