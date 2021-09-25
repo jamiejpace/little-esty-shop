2b92e3bb83131f17ada9437eb3db7279782f8e7d
Jamie Pace <jamiejpace@gmail.com>
2021-09-25 10:34:53 -0600

    Add test for holidays from api

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'bulk discount index page' do
     @discount2 = create :bulk_discount, { merchant_id: @merchant1.id, id: 2 }
     @discount3 = create :bulk_discount, { merchant_id: @merchant1.id, id: 3 }
     @discount4 = create :bulk_discount, { merchant_id: @merchant1.id, id: 4 }
+    @data = {name: "Columbus Day"}
+    @holiday1 = Holiday.new(@data)
   end
 
   it 'lists all bulk discounts with their percentage discount and quantity threshold' do
@@ -19,10 +21,15 @@ RSpec.describe 'bulk discount index page' do
 
   it 'links to each bulk discount show page' do
     visit merchant_bulk_discounts_path(@merchant1)
-
+    save_and_open_page
     expect(page).to have_link("Discount #{@discount1.id}")
     expect(page).to have_link("Discount #{@discount2.id}")
     expect(page).to have_link("Discount #{@discount3.id}")
     expect(page).to have_link("Discount #{@discount4.id}")
+  end
+
+  it 'lists the three next US holidays' do
+    visit merchant_bulk_discounts_path(@merchant1)
+    expect(page).to have_content(@holiday1.name)
   end
 end
