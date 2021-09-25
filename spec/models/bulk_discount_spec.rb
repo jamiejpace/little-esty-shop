8000d65ebaf6a604b4bb094679e1237df2e07a5c
Jamie Pace <jamiejpace@gmail.com>
2021-09-25 16:25:02 -0600

    Add bulk discount validation tests and feature test for new action

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe BulkDiscount, type: :model do
     it { should have_many(:invoice_items).through(:items) }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :percentage_discount }
+    it { should validate_presence_of :quantity_threshold }
+  end
 end
