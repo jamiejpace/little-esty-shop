8b582f637ed56cf55044cdd71f6faa34cb777d44
Jamie Pace <jamiejpace@gmail.com>
2021-09-25 20:27:32 -0600

    Add edit/update actions to bulk discounts controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -8,6 +8,10 @@ class BulkDiscountsController < ApplicationController
     @holidays = BulkDiscountFacade.holidays
   end
 
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
   def new
   end
 
@@ -22,14 +26,34 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+
+  def update
+    bulk_discount = BulkDiscount.find(params[:id])
+    if bulk_discount.update(bulk_discount_nested_params)
+      flash[:success] = 'Successfully Updated Discount'
+      redirect_to merchant_bulk_discount_path(@merchant, bulk_discount)
+    else
+      flash[:alert] = 'Could Not Update - Please Try Again'
+      redirect_to edit_merchant_bulk_discount_path(@merchant, bulk_discount)
+    end
+  end
+
+
   def destroy
     BulkDiscount.find(params[:id]).destroy
     redirect_to merchant_bulk_discounts_path(@merchant)
   end
 
   private
-
   def bulk_discount_params
     params.permit(:name, :percentage_discount, :quantity_threshold)
+  end
+
+  def bulk_discount_nested_params
+    params.require(:bulk_discount).permit(:name, :percentage_discount, :quantity_threshold)
   end
 end
