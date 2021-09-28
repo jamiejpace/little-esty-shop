b4f8f1e95d723b03a19292e7dd9894d3c4ddc92e
Jamie Pace <jamiejpace@gmail.com>
2021-09-28 17:44:01 -0600

    Refactor tests after changes to discount revenue method, add test for invoice items by merchant to model tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -78,5 +78,89 @@ RSpec.describe InvoiceItem, type: :model do
 
       expect(invoice_item_1.revenue_after_discount).to eq(960)
     end
+
+    describe '.discount_revenue' do
+      it 'case 1' do
+        customer = Customer.create(first_name: "Mose", last_name: "Odell", id: 1)
+        merchant_a = Merchant.create(name: "The Gift Shop", id: 1)
+        bulk_discount_a = merchant_a.bulk_discounts.create!(name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+        invoice_a = Invoice.create!(customer_id: customer.id, status: 1, id: 1)
+        item_a = Item.create!(name: "Hat", description: "Good hat", unit_price: 2000, merchant_id: merchant_a.id, status: "enabled", id: 1)
+        item_b = Item.create!(name: "Pants", description: "Good pants", unit_price: 3000, merchant_id: merchant_a.id, status: "enabled", id: 2)
+        InvoiceItem.create!(item_id: item_a.id, invoice_id: invoice_a.id, quantity: 5, unit_price: 2000, status: 2, id: 1)
+        InvoiceItem.create!(item_id: item_b.id, invoice_id: invoice_a.id, quantity: 5, unit_price: 3000, status: 2, id: 2)
+        inv_a_merchant_a_items = invoice_a.invoice_items_by_merchant(merchant_a)
+
+        expect(invoice_a.total_revenue).to eq(25000)
+        expect(inv_a_merchant_a_items.discount_revenue).to eq(25000)
+      end
+
+      it 'case 2' do
+        customer = Customer.create(first_name: "Mose", last_name: "Odell", id: 1)
+        merchant_a = Merchant.create(name: "The Gift Shop", id: 1)
+        bulk_discount_a = merchant_a.bulk_discounts.create!(name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+        invoice_a = Invoice.create!(customer_id: customer.id, status: 1, id: 1)
+        item_a = Item.create!(name: "Hat", description: "Good hat", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 1)
+        item_b = Item.create!(name: "Pants", description: "Good pants", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 2)
+        InvoiceItem.create!(item_id: item_a.id, invoice_id: invoice_a.id, quantity: 10, unit_price: 100, status: 2, id: 1)
+        InvoiceItem.create!(item_id: item_b.id, invoice_id: invoice_a.id, quantity: 5, unit_price: 100, status: 2, id: 2)
+        inv_a_merchant_a_items = invoice_a.invoice_items_by_merchant(merchant_a)
+
+        expect(invoice_a.total_revenue).to eq(1500)
+        expect(inv_a_merchant_a_items.discount_revenue).to eq(1300)
+      end
+
+      it 'case 3' do
+        customer = Customer.create(first_name: "Mose", last_name: "Odell", id: 1)
+        merchant_a = Merchant.create(name: "The Gift Shop", id: 1)
+        bulk_discount_a = merchant_a.bulk_discounts.create!(name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+        bulk_discount_b = merchant_a.bulk_discounts.create!(name: "Discount B", percentage_discount: 30, quantity_threshold: 15)
+        invoice_a = Invoice.create!(customer_id: customer.id, status: 1, id: 1)
+        item_a = Item.create!(name: "Hat", description: "Good hat", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 1)
+        item_b = Item.create!(name: "Pants", description: "Good pants", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 2)
+        InvoiceItem.create!(item_id: item_a.id, invoice_id: invoice_a.id, quantity: 12, unit_price: 100, status: 2, id: 1)
+        InvoiceItem.create!(item_id: item_b.id, invoice_id: invoice_a.id, quantity: 15, unit_price: 100, status: 2, id: 2)
+        inv_a_merchant_a_items = invoice_a.invoice_items_by_merchant(merchant_a)
+
+        expect(invoice_a.total_revenue).to eq(2700)
+        expect(inv_a_merchant_a_items.discount_revenue).to eq(2010)
+      end
+
+      it 'case 4' do
+        customer = Customer.create(first_name: "Mose", last_name: "Odell", id: 1)
+        merchant_a = Merchant.create(name: "The Gift Shop", id: 1)
+        bulk_discount_a = merchant_a.bulk_discounts.create!(name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+        bulk_discount_b = merchant_a.bulk_discounts.create!(name: "Discount B", percentage_discount: 15, quantity_threshold: 15)
+        invoice_a = Invoice.create!(customer_id: customer.id, status: 1, id: 1)
+        item_a = Item.create!(name: "Hat", description: "Good hat", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 1)
+        item_b = Item.create!(name: "Pants", description: "Good pants", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 2)
+        InvoiceItem.create!(item_id: item_a.id, invoice_id: invoice_a.id, quantity: 12, unit_price: 100, status: 2, id: 1)
+        InvoiceItem.create!(item_id: item_b.id, invoice_id: invoice_a.id, quantity: 15, unit_price: 100, status: 2, id: 2)
+        inv_a_merchant_a_items = invoice_a.invoice_items_by_merchant(merchant_a)
+
+        expect(invoice_a.total_revenue).to eq(2700)
+        expect(inv_a_merchant_a_items.discount_revenue).to eq(2160)
+      end
+
+      it 'case 5' do
+        customer = Customer.create(first_name: "Mose", last_name: "Odell", id: 1)
+        merchant_a = Merchant.create(name: "The Gift Shop", id: 1)
+        merchant_b = Merchant.create(name: "Toy Store", id: 2)
+        bulk_discount_a = merchant_a.bulk_discounts.create!(name: "Discount A", percentage_discount: 20, quantity_threshold: 10)
+        bulk_discount_b = merchant_a.bulk_discounts.create!(name: "Discount B", percentage_discount: 30, quantity_threshold: 15)
+        invoice_a = Invoice.create!(customer_id: customer.id, status: 1, id: 1)
+        item_a1 = Item.create!(name: "Hat", description: "Good hat", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 1)
+        item_a2 = Item.create!(name: "Pants", description: "Good pants", unit_price: 100, merchant_id: merchant_a.id, status: "enabled", id: 2)
+        item_b = Item.create!(name: "Lego Tree House", description: "Lego Set", unit_price: 100, merchant_id: merchant_b.id, status: "enabled", id: 3)
+        InvoiceItem.create!(item_id: item_a1.id, invoice_id: invoice_a.id, quantity: 12, unit_price: 100, status: 2, id: 1)
+        InvoiceItem.create!(item_id: item_a2.id, invoice_id: invoice_a.id, quantity: 15, unit_price: 100, status: 2, id: 2)
+        InvoiceItem.create!(item_id: item_b.id, invoice_id: invoice_a.id, quantity: 15, unit_price: 100, status: 2, id: 3)
+        inv_a_merchant_a_items = invoice_a.invoice_items_by_merchant(merchant_a)
+
+        expect(invoice_a.total_revenue).to eq(4200)
+        expect(inv_a_merchant_a_items.discount_revenue).to eq(2010)
+
+      end
+    end
   end
 end
